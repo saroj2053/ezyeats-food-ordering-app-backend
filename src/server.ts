b04b74ae9ebded6e83979ecd2f240445848ee765
Saroj Sah <sarojsaroj390@gmail.com>
2024-08-05 20:44:07 +0200

    changes for deployment

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,11 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "health OK!" });
+});
+
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 
